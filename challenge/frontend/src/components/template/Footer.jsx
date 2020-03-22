657a364738cf15c37c033509e5ea2b928973e3cd
milkfrog <contin.marcelo@gmail.com>
2020-03-22 11:59:05 -0300

    Add css Footer

--- challenge/frontend/src/components/template/Footer.jsx
+++ challenge/frontend/src/components/template/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 import React from 'react'
 
 export default props =>
-    <footer className="footer">
+    <footer className="footer mb-3">
         <span>
             Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Marcelo Contin</strong>
         </span>
