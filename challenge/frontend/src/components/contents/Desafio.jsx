f7f3d89174ca1494f72a41c1e3a1c5bc3aa37877
milkfrog <contin.marcelo@gmail.com>
2020-03-22 19:34:26 -0300

    Add post from front to backend

--- challenge/frontend/src/components/contents/Desafio.jsx
+++ challenge/frontend/src/components/contents/Desafio.jsx
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+import $ from 'jquery'
+import './Desafio.css'
 
-export default props =>
-    <React.Fragment>
-        <main className="content-container-fluid">
-            Desafio
-        </main>
-    </React.Fragment>
+
+const baseUrl = 'http://localhost:4002/divisores'
+const initialState = {
+    values: { 
+        number: '',
+        listDividers: [],
+        isPrime: ''
+    }
+}
+
+
+export default class Desafio extends Component {
+    state = { ...initialState }
+
+    save() {
+        const values = this.state.values
+        const url = baseUrl
+        console.log($.param(values))
+        axios.post(url, $.param(values))
+            .then(resp => {
+                console.log(resp.data.prime)
+                console.log(resp.data.dividers)
+                const values = { ...this.state.values}
+                values.isPrime = resp.data.prime
+                values.listDividers = resp.data.dividers
+                this.setState({ values })
+                console.log(this.state.values)
+            })
+    }
+
+    updateNumber(event) {
+        const values = { ...this.state.values}
+        values[event.target.name] = event.target.value
+        this.setState({ values })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Número</label>
+                            <input type="text" className="form-control" 
+                                name="number"
+                                value={this.state.number}
+                                onChange={e => this.updateNumber(e)}
+                                placeholder="Digite o número Inteiro"/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>É primo?</label>
+                            <input type="text" className="form-control readonly"
+                                name="prime"
+                                value={this.state.values.isPrime}
+                                placeholder="É Primo?"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <div className="form-group">
+                            <label>Lista de divisores</label>
+                            <input type="text" className="form-control readonly"
+                                name="dividers"
+                                value={this.state.values.listDividers}
+                                placeholder="Lista de divisores"/>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Verificar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <main>
+                <div className="container">
+                    <h1 className="mt-5">Pesquisa</h1>
+                    <p>Entre com um número inteiro para descobrir seus divisores e se ele é um número primo</p>
+                    {this.renderForm()}
+                    <h1>Histórico de Pesquisas</h1>
+                </div>
+            </main>
+        )
+    }
+}
--- challenge/frontend/src/components/contents/Home.jsx
+++ challenge/frontend/src/components/contents/Home.jsx
@@ -2,37 +2,35 @@ import React from 'react'
 import './Home.css'
 
 export default props =>
-    <React.Fragment>
-        <main>
-            <div className="container">
-                <h1 className="mt-5">Solução</h1>
-                <p>
-                    Desenvolvido por <strong>Marcelo Pietro Grützmacher Contin</strong>.
-                </p>
-                    <ul>
-                        <li>Para acessar a solução clicar no menu <i>Desafio</i>.</li>
-                        <li>Para conhecer um pouco sobre o autor clicar no menu <i>Autor</i>.</li>
-                    </ul> 
-                <h3 >Definições do Desafio</h3>
-                <p>
-                    Crie um web app que através de um formulário simples, submeta um número e mostre a lista
-                    de divisores desse número e se o mesmo é um número primo.
-                </p>
+    <main>
+        <div className="container">
+            <h1 className="mt-5">Solução</h1>
+            <p>
+                Desenvolvido por <strong>Marcelo Pietro Grützmacher Contin</strong>.
+            </p>
                 <ul>
-                    <li>O sistema deve possuir uma tela com um campo de texto e botão.</li>
-                    <li>Ao clicar no botão, os dados devem ser enviados ao backend para os cálculos.</li>
-                    <li>O resultado deve ser exibido na tela.</li>
-                    <li>Os cálculos dos divisores e do número primo devem ser executados pelo backend.</li>
-                    <li>Estilize as páginas utilizando a ferramenta/biblioteca CSS de sua preferência. 
-                        Nós julgaremos a usabilidade das telas e componentes.</li>
-                    <li>Crie um README explicando as tecnologias e principais bibliotecas utilizadas em sua solução.
-                        Adicione também diretivas de como executar seu projeto localmente.</li>
-                </ul>
-                <h3>Bônus</h3>
-                <ul>
-                    <li>Publique sua solução em um cloud provider (exemplo: Heroku) e nos forneça o link de acesso.</li>
-                    <li>Salve um histórico dos números submetidos.</li>
-                </ul>
-            </div>
-        </main>
-    </React.Fragment>
+                    <li>Para acessar a solução clicar no menu <i>Desafio</i>.</li>
+                    <li>Para conhecer um pouco sobre o autor clicar no menu <i>Autor</i>.</li>
+                </ul> 
+            <h3 >Definições do Desafio</h3>
+            <p>
+                Crie um web app que através de um formulário simples, submeta um número e mostre a lista
+                de divisores desse número e se o mesmo é um número primo.
+            </p>
+            <ul>
+                <li>O sistema deve possuir uma tela com um campo de texto e botão.</li>
+                <li>Ao clicar no botão, os dados devem ser enviados ao backend para os cálculos.</li>
+                <li>O resultado deve ser exibido na tela.</li>
+                <li>Os cálculos dos divisores e do número primo devem ser executados pelo backend.</li>
+                <li>Estilize as páginas utilizando a ferramenta/biblioteca CSS de sua preferência. 
+                    Nós julgaremos a usabilidade das telas e componentes.</li>
+                <li>Crie um README explicando as tecnologias e principais bibliotecas utilizadas em sua solução.
+                    Adicione também diretivas de como executar seu projeto localmente.</li>
+            </ul>
+            <h3>Bônus</h3>
+            <ul>
+                <li>Publique sua solução em um cloud provider (exemplo: Heroku) e nos forneça o link de acesso.</li>
+                <li>Salve um histórico dos números submetidos.</li>
+            </ul>
+        </div>
+    </main>
