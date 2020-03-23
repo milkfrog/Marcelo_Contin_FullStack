908802e83cf07efdaee4888c266703e488ff4d96
milkfrog <contin.marcelo@gmail.com>
2020-03-23 12:51:29 -0300

    Add autor CSS e responsividade

--- challenge/frontend/src/components/contents/Autor.jsx
+++ challenge/frontend/src/components/contents/Autor.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Autor.css'
+import imagemMarcelo from '../assets/imgs/marcelo.png'
+import pdf from '../assets/cv/cv_marcelo_contin.pdf'
 
 export default props =>
         <main>
             <div className="container">
                 <h1 className="mt-5">Links sobre <strong>Marcelo Contin</strong>.</h1>
-                <a href="https://www.linkedin.com/in/continmarcelo/" className="socials" ><i className="fab fa-linkedin"></i></a>
-                <a href="https://www.dropbox.com/s/2ze1oghww46ndjw/CV%20Marcelo%20Contin.pdf?dl=0" className="socials"><i className="far fa-file"></i></a>
-                <img src="challenge/frontend/src/components/assets/imgs/marcelo.png" alt="Foto Marcelo Contin"/>
+                <div className="autor">
+                    <img className="fotoMarcelo" src={imagemMarcelo} alt="Foto Marcelo Contin"/>
+                    <div className="links">
+                        <a href="https://www.linkedin.com/in/continmarcelo/" 
+                            className="socials linkedin" ><i className="fab fa-linkedin"></i>
+                            <span className="texto"> Linkedin</span>
+                        </a>
+                        <a href={pdf} 
+                            className="socials cv"><i className="far fa-file"></i>
+                            <span className="texto"> Currículo</span>
+                        </a>
+                        <a href="https://github.com/milkfrog" 
+                            className="socials github"><i className="fab fa-github"></i>
+                            <span className="texto"> GitHub</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </main>
